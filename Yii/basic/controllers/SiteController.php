ba3ba660b9f6dfc6b14b5fbe28dbaca1af090070
onefiter <853650326@qq.com>
2016-11-17 19:19:45 +0800

    Working with Froms

--- Yii/basic/controllers/SiteController.php
+++ Yii/basic/controllers/SiteController.php
@@ -13,15 +13,22 @@ class SiteController extends Controller
 {
     public function actionSay($message='Hello')
     {
-        // return $this->render ('say', ['message' => $message]);
+         return $this->render ('say', ['message' => $message]);
+    }
+
+    public function actionEntry()
+    {
         $model = new EntryForm;
-        if ($model->load(Yii::$app->request->post()) && model->validate) {
-          //验证$model收到的数据
-          // 做些有意义的事 ...
-          return $this->render('entry-confirm',['model'=>$model]);
-        }else {
-          // 无论是初始化显示还是数据验证错误
-          return $this->render('entry', ['model' => $model]);
+
+        if ($model->load(Yii::$app->request->post()) && $model->validate()) {
+            // 验证 $model 收到的数据
+
+            // 做些有意义的事 ...
+
+            return $this->render('entry-confirm', ['model' => $model]);
+        } else {
+            // 无论是初始化显示还是数据验证错误
+            return $this->render('entry', ['model' => $model]);
         }
     }
 
